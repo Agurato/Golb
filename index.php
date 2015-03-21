7e0ef6fcc213fa8eac87aa9019946c6c1a30363e
JoystX <lucas.nicosia@yahoo.fr>
2015-03-21 18:51:04 +0100

    Avancée du css + structure du HTML

--- index.php
+++ index.php
@@ -3,8 +3,40 @@
 	include('util.inc.php');
 	beginHTML('Golb','css/style.css');
 	?>
-	<h1>Petit titre sympa</h1>
-	<p><img src="img/example-image.jpg" class="img-rounded img-centered"/></p>
+	<div class="container">
+		<div class="header">
+			<h1>Golb</h1>
+			<p class="note">Petit Golb amusant pour les golbers affutés</p>
+		</div>
+		<div class="contentmain">
+			<div class="content">
+				<div class="menu">
+					<ul class="topmenu topmenualign">
+						<li><a href="index.php">Accueil</a></li>
+						<li><a href="index.php">News</a></li>
+						<li><a href="index.php">Messages</a></li>
+						<li><a href="index.php">Profil</a></li>
+						<li><a href="index.php"></a></li>
+
+					</ul>
+				</div>
+			<div>
+				<p>
+					Ici contenu du blog
+					<!-- <img src="img/example-image.jpg" class="img-rounded img-centered" alt="Photo test"/> -->
+				</p>
+			</div>
+			</div>
+			<div class="sidebar">
+
+			</div>
+		</div>
+		<div class="footer">
+			<p>
+				A website by Vincent Monot and Lucas Nicosia
+			</p>
+		</div>
+	</div>
 	<?php
 	endHTML();
 ?>
