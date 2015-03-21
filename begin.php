f6af73560aea650fdfc36e48df81fa7a80b69f8c
JoystX <lucas.nicosia@yahoo.fr>
2015-03-21 16:12:41 +0100

    Ajout des fichiers de base

---
+++ begin.php
@@ -0,0 +1,16 @@
+<?php
+	function beginHTML($title, $css){
+		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+            <title>'.$title.'</title>
+            <meta content="text/html; charset=UTF-8" http-equiv="Content-Type"></meta>
+            <link href="'.$css.'" type="text/css" rel="stylesheet"></link>
+        </head>
+	<body>';
+	}
+	function endHTML(){
+		echo "</body>
+			</html>";
+	}
+?>
