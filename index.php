ef698e177aea7a3d67fe8f7a644ad0eb29d4a0e2
Vincent <vincent@vincent-HP-ENVY-m6-Notebook-PC>
2015-03-22 11:19:01 +0100

    Mise en avant du bloc informations

--- index.php
+++ index.php
@@ -2,41 +2,44 @@
 	include('begin.php');
 	include('util.inc.php');
 	beginHTML('Golb','css/style.css');
-	?>
-	<div class="container">
-		<div class="header">
-			<h1>Golb</h1>
-			<p class="note">Petit Golb amusant pour les golbers affutés</p>
-		</div>
-		<div class="contentmain">
-			<div class="content">
-				<div class="menu">
-					<ul class="topmenu topmenualign">
-						<li><a href="index.php">Accueil</a></li>
-						<li><a href="index.php">News</a></li>
-						<li><a href="index.php">Messages</a></li>
-						<li><a href="index.php">Profil</a></li>
-						<li><a href="index.php"></a></li>
+?>
+		<div class="container">
+			<!-- HEADER -->
+			<div class="header">
+				<h1>Golb</h1>
+				<p class="note">Petit Golb amusant pour les golbers affutés</p>
+			</div>
 
-					</ul>
+			<!-- CONTENT -->
+			<div class="contentmain">
+				<div class="content">
+					<div class="menu">
+						<ul class="topmenu topmenualign">
+							<li><a href="index.php">Accueil</a></li>
+							<li><a href="index.php">News</a></li>
+							<li><a href="index.php">Messages</a></li>
+							<li><a href="index.php">Profil</a></li>
+							<li><a href="index.php">Administrateur</a></li>
+
+						</ul>
+					</div>
+					<div class="info">
+						<p>
+							Ici contenu du blog
+							<!-- <img src="img/example-image.jpg" class="img-rounded img-centered" alt="Photo test"/> -->
+						</p>
+					</div>
 				</div>
-			<div>
-				<p>
-					Ici contenu du blog
-					<!-- <img src="img/example-image.jpg" class="img-rounded img-centered" alt="Photo test"/> -->
-				</p>
-			</div>
-			</div>
-			<div class="sidebar">
+				<div class="sidebar">
 
+				</div>
+			</div>
+
+			<!-- FOOTER -->
+			<div class="footer">
+				<p>A website by Vincent Monot and Lucas Nicosia</p>
 			</div>
 		</div>
-		<div class="footer">
-			<p>
-				A website by Vincent Monot and Lucas Nicosia
-			</p>
-		</div>
-	</div>
-	<?php
+<?php
 	endHTML();
 ?>
