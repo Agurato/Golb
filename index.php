fa0ee6d5dbc915b6d094999e39723a560f442417
Agurato <vincent1611@hotmail.fr>
2015-03-25 18:22:01 +0100

    Login en tant que fenêtre modale

--- index.php
+++ index.php
@@ -2,43 +2,47 @@
 	include('begin.php');
 	include('util.inc.php');
 	beginHTML('Golb','css/style.css');
-	?>
+?>
 	<div class="container">
+		<!-- HEADER -->
 		<div class="header">
-			<div class="login">
-				<form method="get" action="index.php">
-					<p>
-						<label for="login">Login</label>
-						<input type="text" name="login"/>
-						<input class="send" type="submit" name="send" value="Login">						
-					</p>
-					<p>
-						<label for="password">Password</label>
-						<input type="password" name="password" id"=password"/>
-
-					</p>
-
-
-				</form>
-				<form method="post" action="register.php">
-
-				</form>
+			<!-- Login dialog box -->
+			<div id="loginModal" class="modalDialog">
+				<div>
+					<form method="get" action="index.php">
+						<p>
+							<label for="login">Login</label>
+							<input type="text" name="login"/>
+							<input type="submit" class="send" value="Login" />
+						</p>
+						<p>
+							<label for="password">Password</label>
+							<input type="password" name="password" id"=password"/>
+						</p>
+					</form>
+					<a href="#close" title="Close" class="close">Close</a>
+				</div>
 			</div>
+
+			<!-- Title & subtitle -->
 			<div class="titles">
 				<h1>Golb</h1>
 				<p class="note">Petit Golb amusant pour les golbers affutés</p>
 			</div>			
 		</div>
+
+		<!-- CONTENT -->
 		<div class="contentmain">
 			<div class="content">
 				<div class="menu">
-					<ul class="topmenu topmenualign">
-						<li><a href="index.php">Accueil</a></li>
-						<li><a href="index.php">News</a></li>
-						<li><a href="index.php">Messages</a></li>
-						<li><a href="index.php">Profil</a></li>
-						<li><a href="index.php"></a></li>
-
+					<ul class="topmenu">
+						<li class="leftAlign"><a href="index.php">Accueil</a></li>
+						<li class="leftAlign"><a href="index.php">News</a></li>
+						<li class="leftAlign"><a href="index.php">Messages</a></li>
+						<li class="leftAlign"><a href="index.php">Profil</a></li>
+						<li class="rightAlign"><a href="index.php">Administration</a></li>
+						<li class="rightAlign"><a href="index.php">S'inscrire</a></li>
+						<li class="rightAlign"><a href="index.php#loginModal">Se connecter</a></li>
 					</ul>
 				</div>
 			<div>
@@ -52,12 +56,14 @@
 
 			</div>
 		</div>
+
+		<!-- FOOTER -->
 		<div class="footer">
 			<p>
 				A website by Vincent Monot and Lucas Nicosia
 			</p>
 		</div>
 	</div>
-	<?php
+<?php
 	endHTML();
 ?>
